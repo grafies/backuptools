0acf80a13713e713e073ed5981403341086a5d0c
Grafies <1922246955@qq.com>
2023-07-28 14:58:54 +0800

    modified:   GUI/GUI.go

--- GUI/GUI.go
+++ GUI/GUI.go
@@ -1,15 +1,9 @@
 package GUI
 
 import (
-	"fmt"
-	"log"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/driver/desktop"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"gitee.com/grafies/goTypeface"
 )
@@ -25,9 +19,6 @@ func MainGui() {
 		m := fyne.NewMenu("backupTools",
 			fyne.NewMenuItem("主页面", func() {
 				w.Show() //显示主页面
-			}),
-			fyne.NewMenuItem("设置", func() {
-
 			})) //点击此按钮后,重新显示窗口
 		desk.SetSystemTrayMenu(m) //使用捕获的系统托盘菜单
 	}
@@ -37,83 +28,82 @@ func MainGui() {
 }
 
 func Minimize(w fyne.Window) {
-	MainShow2(w) //调用窗口函数
+	title := widget.NewLabel("备份工具")
+	compressedDirectory := widget.NewLabel("压缩目录：")
+	sourceAddress1 := widget.NewEntry() //输入的文本框
 
 	//w.SetContent(container.NewVBox())
 
 }
 
-func MainShow2(w fyne.Window) {
-	title := widget.NewLabel("备份工具")
-	compressedDirectory := widget.NewLabel("压缩目录：")
-	sourceAddress1 := widget.NewEntry() //输入的文本框
+// func MainShow2(w fyne.Window) {
+// 	title := widget.NewLabel("备份工具")
+// 	compressedDirectory := widget.NewLabel("压缩目录：")
+// 	sourceAddress1 := widget.NewEntry() //输入的文本框
 
-	dia1 := widget.NewButton("浏览", func() { //回调函数：打开选择文件对话框
-		fd := dialog.NewFileOpen(func(closer fyne.URIReadCloser, err error) {
-			//fmt.Println(closer.URI().Path())
+// 	dia1 := widget.NewButton("浏览", func() { //回调函数：打开选择文件对话框
+// 		fd := dialog.NewFileOpen(func(closer fyne.URIReadCloser, err error) {
+// 			//fmt.Println(closer.URI().Path())
 
-			if err != nil { //
-				dialog.ShowError(err, w)
-				return
-			}
+// 			if err != nil { //
+// 				dialog.ShowError(err, w)
+// 				return
+// 			} else if closer == nil {
+// 				fmt.Println("取消")
+// 				return
+// 			}
+// 			sourceAddress1.SetText(closer.URI().Path())
 
-			if closer == nil {
-				fmt.Println("取消")
-				return
-			}
-			sourceAddress1.SetText(closer.URI().Path())
+// 		}, w)
 
-		}, w)
+// 		//fd.SetFilter(storage.NewExtensionFileFilter([]string{".txt"})) //打开的文件格式类型
+// 		fd.Show() //显示控件
+// 	})
 
-		//fd.SetFilter(storage.NewExtensionFileFilter([]string{".txt"})) //打开的文件格式类型
-		fd.Show() //显示控件
-	})
+// 	text := widget.NewMultiLineEntry() //多行输入组件
+// 	text.Disable()                     //禁用输入框，不能更改数据
 
-	text := widget.NewMultiLineEntry() //多行输入组件
-	text.Disable()                     //禁用输入框，不能更改数据
+// 	storageDirectory := widget.NewLabel("存放目录：")
+// 	sourceAddress2 := widget.NewEntry() //输入的文本框
 
-	storageDirectory := widget.NewLabel("存放目录：")
-	sourceAddress2 := widget.NewEntry() //输入的文本框
+// 	dia2 := widget.NewButton("浏览", func() {
+// 		dialog.ShowFolderOpen(func(uri fyne.ListableURI, err error) {
+// 			if err != nil {
+// 				dialog.ShowError(err, w)
+// 				return
+// 			} else if uri != nil {
+// 				log.Println("取消")
 
-	dia2 := widget.NewButton("浏览", func() {
-		dialog.ShowFolderOpen(func(uri fyne.ListableURI, err error) {
-			if err != nil {
-				dialog.ShowError(err, w)
-				return
-			}
-			if uri != nil {
-				log.Println("取消")
+// 			}
 
-			}
+// 			sourceAddress2.SetText(uri.Path()) //设置输入框内容
+// 		}, w)
+// 	})
 
-			sourceAddress2.SetText(uri.Path()) //设置输入框内容
-		}, w)
-	})
+// 	dia3 := widget.NewButton("开始压缩", func() {
+// 		go func() {
+// 			//获取内容
+// 			if compressedDirectory.Text != "" && storageDirectory.Text != "" {
+// 				text.SetText("")
+// 				text.Refresh()
+// 			}
+// 		}()
+// 	})
 
-	dia3 := widget.NewButton("开始压缩", func() {
-		go func() {
-			//获取内容
-			if compressedDirectory.Text != "" && storageDirectory.Text != "" {
-				text.SetText("")
-				text.Refresh()
-			}
-		}()
-	})
+// 	head := container.NewCenter(title)
 
-	head := container.NewCenter(title)
+// 	//第一行和第二行
+// 	v1 := container.NewBorder(layout.NewSpacer(), layout.NewSpacer(), compressedDirectory, dia1, sourceAddress1)
+// 	v2 := container.NewBorder(layout.NewSpacer(), layout.NewSpacer(), storageDirectory, dia2, sourceAddress2)
 
-	//第一行和第二行
-	v1 := container.NewBorder(layout.NewSpacer(), layout.NewSpacer(), compressedDirectory, dia1, sourceAddress1)
-	v2 := container.NewBorder(layout.NewSpacer(), layout.NewSpacer(), storageDirectory, dia2, sourceAddress2)
+// 	v3 := container.NewHBox(dia3)       //显示第三行按钮
+// 	v3Center := container.NewCenter(v3) //第三行
 
-	v3 := container.NewHBox(dia3)       //显示第三行按钮
-	v3Center := container.NewCenter(v3) //第三行
+// 	labelLast := widget.NewLabel("backupTools")
 
-	labelLast := widget.NewLabel("backupTools")
+// 	position := container.NewVBox(head, v1, v2, v3Center, text, labelLast) //控制显示位置顺序
 
-	position := container.NewVBox(head, v1, v2, v3Center, text, labelLast) //控制显示位置顺序
+// 	w.SetContent(position)
+// 	w.ShowAndRun() //显示主页面
 
-	w.SetContent(position)
-	w.ShowAndRun() //显示主页面
-
-}
+// }
